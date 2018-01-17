c9155842d6e80e2dc3455b3d6f16626f8db69b67
James Lucas <jamesmlucas@gmail.com>
2018-01-16 21:56:38 -0600

    more debugging

--- smartcam/video_writer.py
+++ smartcam/video_writer.py
@@ -50,9 +50,13 @@ class FfmpegVideoWriter(VideoWriter):
       logger.debug("writing frame")
       nonlocal last_frame
       if frame is None:
-        logger.debug("received null frame")
+        t1 = datetime.datetime.now()
         p.stdin.close()
+        t2 = datetime.datetime.now()
+        logger.debug("Time to close pipe stdin: %s" % (t2-t1))
         p.wait()
+        t3 = datetime.datetime.now()
+        logger.debug("Time to p.wait(): %s" % (t3-t2))
         local_video = LocalVideo(start_frame.id,
           start_frame.time,
           last_frame.time,
@@ -62,16 +66,20 @@ class FfmpegVideoWriter(VideoWriter):
         )
         if self.cloud_writer is not None:
           try:
+            t4 = datetime.datetime.now()
             self.cloud_writer.write_video(local_video)
+            t5 = datetime.datetime.now()
+            logger.debug("Time for cloud_write.write_video: %s" % (t5-t4))
           except Exception as e:
             logger.error("Failed to write video to cloud_writer: %s" % e)
         return
       last_frame = frame
-      logger.debug("Encoding frame")
       buf = frame.encode()
       buf.seek(0)
-      logger.debug("Writing video frame to pipe")
+      t6 = datetime.datetime.now()
       p.stdin.write(buf.read())
+      t7 = datetime.datetime.now()
+      logger.debug("Time write video frame to pipe: %s" % (t7-t6))
 
     return writer, full_path
 
@@ -83,6 +91,7 @@ class FfmpegVideoWriter(VideoWriter):
         frame = self.queue.get()
       except queue.Empty:
         continue
+      t1 = datetime.datetime.now()
       if frame is None:
         if writer is not None:
           logger.debug("writing null frame")
@@ -92,15 +101,20 @@ class FfmpegVideoWriter(VideoWriter):
           try:
             logger.debug("deleting %s" % video_file)
             os.remove(video_file)
+            t3 = datetime.datetime.now()
           except Exception as e:
             logger.error("Failed to delete video")
             raise e
         continue
       if writer is None:
         logger.debug('instantiating writer')
+        t4 = datetime.datetime.now()
         writer, video_file = self.get_writer(frame, ext='mp4')
+        t5 = datetime.datetime.now()
       try:
+        t6 = datetime.datetime.now()
         writer(frame)
+        t7 = datetime.datetime.now()
       except Exception as e:
         logger.error("Failed to write frame to video ")
         raise e
